83d5cd52cc0dfd0aebd5840b952206d9d830044c
rajshekarm <rajshekarmg97@gmail.com>
2025-10-11 16:23:33 -0500

    added page to view agent detail by id

---
+++ src/app/(dashboard)/agents/[agentId]/page.tsx
@@ -0,0 +1,20 @@
+import { AgentIdView } from "@/modules/agents/ui/views/agent-id-view";
+
+interface Props {
+  params: { agentId: string };
+}
+
+const Page = async ({ params }: Props) => {
+
+    console.log("AgentIdView page rendering for agentId:");
+  const { agentId } = await params;
+
+  
+  return (
+    <div>
+      <AgentIdView agentId={agentId} />
+    </div>
+  );
+};
+
+export default Page;
